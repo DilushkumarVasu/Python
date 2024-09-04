39f1004944181417a1cc536fa0d7de2bdbf22834
DilushkumarVasu <dilushvasu1996@gmail.com>
2024-09-04 20:11:49 +0530

    ifelse practical

---
+++ ifelse.py
@@ -0,0 +1,92 @@
+'''
+mark = int(input())
+
+if(mark>35):
+    print("Pass")
+else:
+    print("Fail")
+'''
+
+'''
+income = int(input("Income:"))
+
+if(income > 7000):
+    print("Not eligible for scholarship")
+else:
+    print("Eligible for scholarship")
+'''
+
+'''
+number = int(input())
+
+if(number % 3 == 0 and number % 5 == 0):
+    print("Divisible by 3 and 5")
+else:
+    print("Not divisible by 3 and 5")
+'''
+
+'''
+num1 = int(input())
+if(num1%2==0):
+    print("Even")
+else:
+    print("Odd")
+'''
+
+'''
+score = int(input())
+if(score<35):
+    print("Poor Student")
+elif(score>35 and score<70):
+    print("Average Student")
+elif(score>70 and score <=100):
+    print("Good Student")
+else:
+    print("Give a valid score from 0 to 100")
+'''
+
+'''
+a = int(input("a:"))
+b = int(input("b:"))
+opr = input("operator:")
+
+if(opr == "+"):
+    print(a+b)
+elif(opr == "-"):
+    print(a-b)
+elif(opr == "*"):
+    print(a*b)
+elif(opr == "/"):
+    print(a/b)
+elif(opr == "%"):
+    print(a%b)
+else:
+    print("Invalid operator")
+'''
+
+'''
+score_per = int(input())
+if(score_per >= 70):
+    name = input("Name:")
+    department = input("Department:")
+    location = input("Location:")
+    print("You are eligible")
+else:
+    print("You are not eligible")
+'''
+
+'''
+salary = int(input("Salary:"))
+age = int(input("Age:"))
+
+if(salary >= 20000 or age <= 25):
+    loan = int(input("Loan amount:"))
+    if(loan <= 50000):
+        print("You are eligible for loan")
+    else:
+        print("Maximum loan amount is 50000")
+else:
+    print("Not eligible for loan")
+'''
+
+
